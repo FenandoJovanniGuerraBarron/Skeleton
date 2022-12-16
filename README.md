8b33bfaa3a7cb98cce71f12d6974cf1027e9bc50
FernandoJovanniGuerraBarron <fernando_jova@hotmail.com>
2022-12-15 20:51:56 -0600

    null

---
+++ README.md
@@ -0,0 +1,12 @@
+Punto de partida
+
+Cuando queremos recuperar una contraseña hago una peticion que me pide el email
+
+- post creara una nuevo recovery password
+    - Llegar el correo electronico
+    - Link que te permite hacer una peticion para actualizar tus datos
+
+- patch para atualizar la contraseña validando el enlace del recovery password
+    -cuando mandamos la nueva contraseña pasan dos cosas
+        -se modifica la contraseña
+        -se modifica el recoverytoken como ya usado, para que no se pueda a volver a utilizar
--- src/auth/auth.controllers.js
+++ src/auth/auth.controllers.js
@@ -1,17 +1,36 @@
+const uuid = require('uuid')
 const { findUserByEmail } = require('../users/users.controllers')
 const { comparePassword } = require('../utils/crypto')
+const RecoveryPassword = require('../models/recoveryPasswords.models')
 
 const checkUsersCredentials = async (email, password) => {
     try {
         const user = await findUserByEmail(email)
         const verifyPassword = comparePassword(password, user.password)
-        if(verifyPassword){
+        if (verifyPassword) {
             return user
-        } 
+        }
         return null
     } catch (error) {
-        return error
+        return null
     }
 }
 
-module.exports = checkUsersCredentials
+const createRecoveryToken = async (email) => {
+    try {
+        const user = await findUserByEmail(email)
+        const data = await RecoveryPassword.create({
+            id:uuid.v4(),
+            userId:user.id
+        })
+        return data
+    } catch (error) {
+        return null
+    }
+
+}
+
+module.exports = {
+    checkUsersCredentials,
+    createRecoveryToken
+}
